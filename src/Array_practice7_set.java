a2f314ecd8bd0f7a92c06212cd53f8b1da100766
Sumalatha <sumalatha2032003@gmail.com>
2025-08-20 22:58:52 +0530

    First commit -added Array_practice7_set.java

---
+++ src/Array_practice7_set.java
@@ -0,0 +1,14 @@
+public class Array_practice7_set {
+    public static void main(String[] args) {
+        //java program to find the minimum element in an array...
+        int[] min={92,67,35,45,120,105,100,98,78,65};
+        System.out.println("The minimum number in the given array is:");
+        int arr1=min[2];
+        for(int i=0;i<=min.length;i++){
+            if(arr1==min[2]){
+                System.out.println(min[2]);
+                break;
+            }
+        }
+    }
+}
