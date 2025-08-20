1b5c373de78ea8cb339ca755bbc43703f524acee
Sumalatha <sumalatha2032003@gmail.com>
2025-08-20 22:58:07 +0530

    First commit -added Array_practice6_set.java

---
+++ src/Array_practice6_set.java
@@ -0,0 +1,14 @@
+public class Array_practice6_set {
+    public static void main(String[] args) {
+        //java program to find the maximum element in an array...
+        int[] max={92,67,35,45,120,105,100,98,78,65};
+        System.out.println("The maximum number in the given array is:");
+        int arr=max[4];
+        for(int i=0;i<=max.length;i++){
+            if(arr==max[4]){
+                System.out.println(max[4]);
+                break;
+            }
+        }
+    }
+}
