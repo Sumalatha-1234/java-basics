445b8dc10628364f64256acd76bb7bc6f0deedd3
Sumalatha <sumalatha2032003@gmail.com>
2025-08-21 22:05:54 +0530

    First commit -added presint.java

---
+++ src/presint.java
@@ -0,0 +1,10 @@
+//Write a java program to detect whether a number entered by the
+// user is integer or not....
+import java.util.Scanner;
+public class presint {
+    public static void main(String[] args) {
+        System.out.println("Enter your number:");
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.hasNext());
+    }
+}
