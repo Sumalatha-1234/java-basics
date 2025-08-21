79266133731a0c64c05e6b6ca2bc5d67896d6140
Sumalatha <sumalatha2032003@gmail.com>
2025-08-21 22:04:41 +0530

    First commit -added ID.java

---
+++ src/ID.java
@@ -0,0 +1,10 @@
+//increment and decrement....
+public class ID {
+    public static void main(String[] args) {
+        int i=56;
+        System.out.println(i++);//first it returns the value then increment number..
+        System.out.println(i);
+        System.out.println(++i);//it first increment the number then returns number..
+        System.out.println(i);
+    }
+}
