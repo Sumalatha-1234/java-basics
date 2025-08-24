1f69a9611a6f82cdf06fb94566926d1a411e8818
Sumalatha <sumalatha2032003@gmail.com>
2025-08-24 19:13:21 +0530

    First commit -added patternA.java

--- src/multiReverse.java
+++
@@ -1,9 +0,0 @@
-//Write a program to print the multiplication table of 10 in reverse order...
-public class multiReverse {
-    public static void main(String[] args) {
-        int n=10;
-        for(int i=10;i>=1;i--){
-            System.out.printf("%d X %d = %d\n",n,i,n*i);
-        }
-    }
-}
--- src/multiplicationTable.java
+++
@@ -1,9 +0,0 @@
-//Write a program to print multiplication table of a given number n...
-public class multiplicationTable {
-    public static void main(String[] args) {
-        int n=12;
-        for(int i=1;i<=10;i++){
-            System.out.printf("%d X %d = %d\n",n,i,n*i);
-        }
-    }
-}
