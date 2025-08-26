c9a8cffe3e79c2958808e534bad9cab6aff7e6a3
Sumalatha <sumalatha2032003@gmail.com>
2025-08-26 16:34:03 +0530

    First commit -added MetOver.java

---
+++ src/MetOver.java
@@ -0,0 +1,11 @@
+//A Simple method overloading program...
+package com.company;
+public class MetOver {
+    static void tellJoke(){
+        System.out.println("I invented a new word!\n"+"plagiarism");
+    }
+
+    public static void main(String[] args) {
+        tellJoke();
+    }
+}
