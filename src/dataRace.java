2002129c81b936d04c2af0d5536f6a49d04a2de3
Sumalatha <sumalatha2032003@gmail.com>
2025-08-29 16:29:17 +0530

    First commit -added dataRace.java

---
+++ src/dataRace.java
@@ -0,0 +1,18 @@
+//Data race example....
+public class dataRace extends Thread{
+    public static int x;
+    public void run(){
+        for(int i=0;i<100;i++){
+            x=x+1;
+            x=x-1;
+        }
+    }
+
+    public static void main(String[] args) {
+        x=0;
+        for(int i=0;i<1000;i++){
+            new dataRace().start();
+            System.out.println(x);//x not always 0!
+        }
+    }
+}
