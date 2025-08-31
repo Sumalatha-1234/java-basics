fc3ee7afa25267995ceb938f0340bd3437115970
Sumalatha <sumalatha2032003@gmail.com>
2025-08-31 20:57:04 +0530

    First commit -added NumberPattern.java

---
+++ src/NumberPattern.java
@@ -0,0 +1,37 @@
+/*
+Use nested loops and a method to print the following pattern (for n=5):
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+ */
+import java.util.Scanner;
+
+public class NumberPattern {
+    static void printPattern(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        printPattern(n);
+    }
+}
+/*
+output:
+Enter n: 5
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+ */
