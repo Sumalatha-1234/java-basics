cd0b7841b0380158a23b2b71c0c92ead105887c8
Sumalatha <sumalatha2032003@gmail.com>
2025-09-02 23:51:34 +0530

    First commit -added ConstructorOrderDemo.java

---
+++ src/sa.java
@@ -0,0 +1,5 @@
+/*Create a Shape class with a parameterized constructor (e.g., name of shape).
+Create Rectangle and Square subclasses that call the Shape constructor and add their
+own dimensions.
+ */
+// Parent class
